9fd2ff186e1d07dbbba03a676ee76d2a6214d16c
namitg105 <namitg105@gmail.com>
2026-08-01 14:43:09 +0530

    Fix Unauthenticated return in AuthWrapper to return SplashScreen (Get Started screen)

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ import 'package:noteswap/features/auth/data/firebase_auth_repo.dart';
 import 'package:noteswap/features/auth/presentation/cubits/auth_cubit.dart';
 import 'package:noteswap/features/auth/presentation/cubits/auth_states.dart';
 import 'package:noteswap/features/auth/presentation/pages/SplashScreen.dart';
-import 'package:noteswap/Views/Onboarding/OnboardingOne.dart';
-import 'package:noteswap/features/auth/presentation/pages/auth_page.dart';
 import 'package:noteswap/features/private_chat/data/private-chat-services/user_service.dart';
 import 'package:noteswap/firebase_options.dart';
 import 'ViewModels/DarkModeViewModels.dart'; // Retained member's theme controller
